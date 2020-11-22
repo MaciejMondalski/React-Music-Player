bd4df3dbde513d4989ca5dfa8d785f926835d99e
Maciej Mondalski <maciej.mondalski@gmail.com>
2020-11-22 23:50:00 +0100

    first react commit

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+
+const Nav = ({ libraryStatus, setLibraryStatus }) => {
+  return (
+    <nav>
+      <h1>Reactify</h1>
+      <button
+        className={`${libraryStatus ? "active-button" : ""}`}
+        onClick={() => setLibraryStatus(!libraryStatus)}
+      >
+        Library
+        <FontAwesomeIcon icon={faMusic} />
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
